8bdbf937ae6786f48ebb1027bd95f2f6e61d37c5
AlinaTlaneci <42754478+AlinaTlaneci@users.noreply.github.com>
2020-12-11 13:51:02 -0600

    files
    files

---
+++ app/src/main/java/com/ness/marsianito/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ness.marsianito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
